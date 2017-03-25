9365a97910219cd44e24309f019079fdccebe825
dylandegrace <dylan.degrace@gmail.com>
2017-03-24 22:14:12 -0300

    embedtest agan

--- games/games.py
+++ games/games.py
@@ -146,25 +146,23 @@ class Games:
                                                              server.id))
             gameRemoveSuccess = "Game **{}** successfully removed. You have lost access to those channels.".format(rolename)
             await self.bot.say(gameRemoveSuccess)
+			
+    @commands.command(no_pm=True, pass_context=True)
+    async def embedtest(self, ctx):
+        
+        author_name = ctx.message.author
+        description = ("Short little description with a link to google "
+                   "(https://www.google.com")
+        field_name = "Generic Name"
+        field_contents = "Example contents for this field"
+        footer_text = "Hi. I am a footer text."
 
-    @commands.command(pass_context=True)
-    async def mycommand(self, ctx):
-
-    author_name = ctx.message.author
-    description = ("Short little description with a link to "
-                   "the [guide](https://github.com/Redjumpman/Jumper-Cogs/wiki/Discord-Coding-Guide)")
-    field_name = "Generic Name"
-    field_contents = "Example contents for this field"
-    footer_text = "Hi. I am a footer text. I look small when displayed.
-
-    embed = discord.Embed(colour=0xFF0000, description=description) # Can use discord.Colour() as well
-    embed.title = "Cool title for my embed"
-    embed.set_author(name=str(author.name), icon_url=author.avatar_url)
-    embed.add_field(name=field_name, value=field_contents) # Can add multiple fields.
-    embed.set_footer(text=footer_text)
-
-    await self.bot.say(embed=embed)
-
+        embed = discord.Embed(colour=0xFF0000, description=description) # Can use discord.Colour() as well
+        embed.title = "i am the title"
+        embed.set_author(name=str(author.name), icon_url=author.avatar_url)
+        embed.add_field(name=field_name, value=field_contents) # Can add multiple fields.
+        embed.set_footer(text=footer_text)
+        await self.bot.say(embed=embed)		
 
 
 def check_files():
