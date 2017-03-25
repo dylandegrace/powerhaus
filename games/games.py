d676237e1b64138294b2553ec64d3068fb835e83
dylandegrace <dylan.degrace@gmail.com>
2017-03-25 00:30:49 -0300

    edsit

--- games/games.py
+++ games/games.py
@@ -169,7 +169,8 @@ class Games:
 
         embed = discord.Embed(colour=0xdb941a) # Can use discord.Colour() as well
         embed.title = "__**GAMES LIST**__"
-        embed.add_field(name="\nWe support the following games in our Discord server with private channels:\n", value="```\n"+role_names+"```") # Can add multiple fields.
+		embed.description = "We support the following games in our Discord server with private channels:\n"
+        embed.add_field(name="", value="```\n"+role_names+"```") # Can add multiple fields.
         embed.set_footer(text=footer_text)
         await self.bot.say(embed=embed)
 
