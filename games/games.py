c21e93a365f17322c70d69d39f5d7ed31bde0fe6
dylandegrace <dylan.degrace@gmail.com>
2017-04-01 16:06:41 -0300

    wow -> woah

--- games/games.py
+++ games/games.py
@@ -81,7 +81,7 @@ class Games:
 
     @commands.group(no_pm=True, pass_context=True, invoke_without_command=True)
     async def addgame(self, ctx, *, rolename):
-        """Wow there partner, you forgot to add a game at the end of the command.
+        """Woah there partner, you forgot to add a game at the end of the command.
         Review the list with !games and append them (individually) to your !addgame command
 
         For Admins: you can add additional games using !gameset"""
