95646420ff409a6f2cc960927e7c3fb428bfff27
dylandegrace <dylan.degrace@gmail.com>
2017-04-05 21:18:24 -0300

    name

--- games/games.py
+++ games/games.py
@@ -93,11 +93,11 @@ class Games:
         recruitRole = discord.utils.get(ctx.message.server.roles, name="Recruit")
         if (recruit and recruit[0].name == "Recruit"):
             await self.bot.add_roles(author, memberRole)
-            await self.bot.say("Adding member role: "+memberRole)
+            await self.bot.say("Adding member role: "+memberRole.name)
             await self.bot.say("tic...")
             await asyncio.sleep(0.5)
             await self.bot.say("...toc")
-            await self.bot.remove_roles(author, recruitRole)
+            await self.bot.remove_roles(author, recruitRole.name)
             await self.bot.say("Removing recruit role: "+recruitRole)
 			
             await self.bot.whisper("Thank you for accepting the rules.")
