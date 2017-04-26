8e46ade15007041b25963bc0956e91b140080be4
dylandegrace <dylan.degrace@gmail.com>
2017-04-26 11:18:47 -0300

    ctx

--- phroles/phroles.py
+++ phroles/phroles.py
@@ -16,7 +16,7 @@ class CustomRoles:
             await send_cmd_help(context)
 
     @_role.command(pass_context=True, no_pm=True, name='add', aliases=['new'])
-    @checks.role_or_permissions(check="Division Lead")
+    @checks.role_or_permissions(ctx, check="Division Lead")
     async def _add(self, context, color, *role_name):
         """Add a role
         Example: role add ff0000 Red Role"""
