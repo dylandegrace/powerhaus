558cf43b4f2f6854c5efb9a64cd8baf494cd52b8
dylandegrace <dylan.degrace@gmail.com>
2017-05-25 21:39:01 -0300

    ==

--- phroles/phroles.py
+++ phroles/phroles.py
@@ -120,7 +120,7 @@ class CustomRoles:
         message = '\n'
 		
         for role in server.roles:
-            if role.name = 'Member':
+            if role.name == 'Member':
                 messagetotal = '\n{} ({})'.format(role.name, len([member for member in server.members if ([r for r in member.roles if r.name == role.name])]))
             if role.permissions.value < 1 and role.name != '@everyone':
                 message += '\n{} ({})'.format(role.name, len([member for member in server.members if ([r for r in member.roles if r.name == role.name])]))
