cfac03e3e4609886023fa57d37d91fea228d326f
dylandegrace <dylan.degrace@gmail.com>
2017-06-07 20:19:57 -0300

    added team and list

--- phroles/phroles.py
+++ phroles/phroles.py
@@ -11,7 +11,7 @@ class CustomRoles:
 
     @commands.group(pass_context=True, no_pm=True, name='role')
     async def _role(self, context):
-        """DLs can add roles, users can apply or relieve roles. Roles created with this cog have no permissions."""
+        """Shows a list of the teams or games we currently have within the server"""
         if context.invoked_subcommand is None:
             await send_cmd_help(context)
 
@@ -111,7 +111,7 @@ class CustomRoles:
             # message = 'There is no such role on this server'
         # await self.bot.say(message)
 
-    @_role.command(pass_context=True, no_pm=True, name='games')
+    @_role.command(pass_context=True, no_pm=True, name='listgames')
     @checks.mod_or_permissions(manage_roles=True)
     async def _games(self, context):
         """List all available games and their member count"""
@@ -132,7 +132,7 @@ class CustomRoles:
         embed.add_field(name="People have added the following games:", value=message) # Can add multiple fields.
         await self.bot.say(embed=embed)
 		
-    @_role.command(pass_context=True, no_pm=True, name='teams')
+    @_role.command(pass_context=True, no_pm=True, name='listteams')
     @checks.mod_or_permissions(manage_roles=True)
     async def _teams(self, context):
         """List all current team roles and their member count"""
@@ -152,7 +152,37 @@ class CustomRoles:
         embed.add_field(name="All available team roles are as follows:", value=message) # Can add multiple fields.
         await self.bot.say(embed=embed)
 
+    @commands.group(pass_context=True, no_pm=True, name='team')
+    async def _team(self, context):
+        """DLs can add teams, Team Managers/DLs+ can apply teams to users. Teams created with this cog have no permissions."""
+        if context.invoked_subcommand is None:
+            await send_cmd_help(context)
+			
+    @_team.command(pass_context=True, no_pm=True, name='add', aliases=['new'])
+    async def _add(self, context, color, *role_name):
+        """Add a role
+        Example: role add ff0000 Red Role"""
+        server = context.message.server
 
+        lead_role = "Division Lead"
+        lead_check = lambda r: r.name.lower() == lead_role.lower()
+        test = checks.role_or_permissions(context, lead_check)
+				
+        if re.search(r'^(?:[0-9a-fA-F]{3}){1,2}$', color):
+            name = ' '.join(role_name)
+            color = discord.Color(int(color, 16))
+            permissions = discord.Permissions(permissions=0)
+            try:
+                await self.bot.create_role(server, name=name, color=color, permissions=permissions, hoist=False)
+                message = 'New role made'
+            except discord.Forbidden:
+                message = 'I have no permissions to do that. Please give me role managing permissions.'
+        else:
+            message = '`Not a valid heximal color`'
+        await self.bot.say(message)
+        await self.bot.say("context, lead_check  ==" + str(test))			
+		
+		
 def setup(bot):
     n = CustomRoles(bot)
     bot.add_cog(n)
