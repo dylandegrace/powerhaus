9207beff8d68fdb011938077206d9acaa11f777e
dylandegrace <dylan.degrace@gmail.com>
2017-06-07 21:30:03 -0300

    condcheck

--- phroles/phroles.py
+++ phroles/phroles.py
@@ -171,11 +171,11 @@ class CustomRoles:
         test = checks.role_or_permissions(context, lead_check)
         test2 = checks.role_or_permissions(context, manager_check)
         
-        if (lead_check and manager_check):
+        if ((lead_check and manager_check) == True):
             message = "I am a Division Lead and Team Manager"
-        elif (lead_check and not manager_check):
+        elif (lead_check == True and manager_check == False):
             message = "I am only a Division Lead"
-        elif (not lead_check and manager_check):
+        elif (lead_check == False and manager_check == True):
             message = "I am only a Team Manager"
         else:
             message = "I am neither"
