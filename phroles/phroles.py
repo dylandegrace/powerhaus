77f9e98ae9efa8d98a75e7a4301db452e2230859
dylandegrace <dylan.degrace@gmail.com>
2017-06-29 21:35:47 -0300

    added mm command

--- phroles/phroles.py
+++ phroles/phroles.py
@@ -2,11 +2,26 @@ import re
 import discord
 from .utils import checks
 from discord.ext import commands
+from random import choice
+import itertools
+from cogs.utils.chat_formatting import box
+from cogs.utils.chat_formatting import pagify
 from __main__ import send_cmd_help
+
+BOTCOMMANDER_ROLE = ["Chief Officer"]
 
 class POWERHAUSRoles:
     def __init__(self, bot):
         self.bot = bot
+		
+    def grouper(self, n, iterable, fillvalue=None):
+        """
+        Helper function to split lists
+        Example:
+        grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx
+        """
+        args = [iter(iterable)] * n
+        return ([e for e in t if e != None] for t in itertools.zip_longest(*args))    
 		
     @commands.group(pass_context=True, no_pm=True, name='role')
     async def _role(self, context):
@@ -237,8 +252,269 @@ class POWERHAUSRoles:
         # else:
             # message = 'There is no such role on this server'
         # await self.bot.say(message)
-		
-		
+
+    @commands.command(pass_context=True)
+    @commands.has_any_role(*BOTCOMMANDER_ROLE)
+    async def mm(self, ctx, *args):
+        """
+        Member management command.
+        Get a list of users that satisfy a list of roles supplied.
+        e.g.
+        !mm S M -L
+        !mm +S +M -L
+        fetches a list of users who has the roles S, M but not the role L.
+        S is the same as +S. + is an optional prefix for includes.
+        Optional arguments
+        --output-mentions
+            Append a string of user mentions for users displayed.
+        --output-id
+            Append a string ot user ids for the results.
+        --output-mentions-only
+            Don’t display the long list and only display the list of member mentions
+        --members-without-clan-tag
+            RACF specific option. Equivalent to typing Member -Alpha -Bravo -Charlie -Delta -Echo -Foxtrot -Golf -Hotel
+        --list
+            Display as inline list
+        --only-role
+            Check member has one and exactly one role
+        --everyone
+            Include everyone
+        """
+
+        # Extract optional arguments if exist
+        option_output_mentions = "--output-mentions" in args
+        option_output_id = "--output-id" in args
+        option_output_mentions_only = "--output-mentions-only" in args
+        option_members_without_clan_tag = "--members-without-clan-tag" in args
+        option_sort_join = "--sort-join" in args
+        option_everyone = "--everyone" in args
+        option_sort_alpha = "--sort-alpha" in args
+        option_csv = "--csv" in args
+        option_list = "--list" in args
+        option_only_role = "--only-role" in args
+
+        server = ctx.message.server
+        server_roles_names = [r.name.lower() for r in server.roles]
+
+        # get list of arguments which are valid server role names
+        # as dictionary {flag, name}
+        out = ["**Member Management**"]
+
+        if option_members_without_clan_tag:
+            args = ['Member', '-Alpha', '-Bravo', '-Charlie',
+                    '-Delta', '-Echo',
+                    '-Foxtrot', '-Golf', '-Hotel',
+                    '-eSports', '-Special']
+
+        role_args = []
+        flags = ['+', '-']
+        if args is not None:
+            for arg in args:
+                has_flag = arg[0] in flags
+                flag = arg[0] if has_flag else '+'
+                name = arg[1:] if has_flag else arg
+
+                if name.lower() in server_roles_names:
+                    role_args.append({'flag': flag, 'name': name.lower()})
+
+        plus  = set([r['name'] for r in role_args if r['flag'] == '+'])
+        minus = set([r['name'] for r in role_args if r['flag'] == '-'])
+
+        # Used for output only, so it won’t mention everyone in chat
+        plus_out = plus.copy()
+
+        if option_everyone:
+            plus.add('@everyone')
+            plus_out.add('everyone')
+
+        help_str = (
+            'Syntax Error: You must include at '
+            'least one role to display results.')
+
+        if len(plus) < 1:
+            out.append(help_str)
+        else:
+            out.append("Listing members who have these roles: {}".format(
+                ', '.join(plus_out)))
+        if len(minus):
+            out.append("but not these roles: {}".format(
+                ', '.join(minus)))
+
+        await self.bot.say('\n'.join(out))
+
+        # only output if argument is supplied
+        if len(plus):
+            # include roles with '+' flag
+            # exclude roles with '-' flag
+            out_members = set()
+            for m in server.members:
+                roles = set([r.name.lower() for r in m.roles])
+                if option_everyone:
+                    roles.add('@everyone')
+                exclude = len(roles & minus)
+                if not exclude and roles >= plus:
+                    out_members.add(m)
+
+            # only role
+            if option_only_role:
+                out_members = [m for m in out_members if len(m.roles) == 2]
+
+            suffix = 's' if len(out_members) > 1 else ''
+            await self.bot.say("**Found {} member{}.**".format(
+                len(out_members), suffix))
+
+            # sort join
+            out_members = list(out_members)
+            out_members.sort(key=lambda x: x.joined_at)
+
+            # sort alpha
+            if option_sort_alpha:
+                out_members = list(out_members)
+                out_members.sort(key=lambda x: x.display_name)
+
+            # embed output
+            if not option_output_mentions_only:
+                if option_csv:
+                    for page in pagify(
+                            self.get_member_csv(out_members), shorten_by=50):
+                        await self.bot.say(page)
+                elif option_list:
+                    for page in pagify(
+                            self.get_member_list(out_members), shorten_by=50):
+                        await self.bot.say(page)
+                else:
+                    for data in self.get_member_embeds(ctx, out_members):
+                        try:
+                            await self.bot.say(embed=data)
+                        except discord.HTTPException:
+                            await self.bot.say(
+                                "I need the `Embed links` permission "
+                                "to send this")
+
+            # Display a copy-and-pastable list
+            if option_output_mentions | option_output_mentions_only:
+                mention_list = [m.mention for m in out_members]
+                await self.bot.say(
+                    "Copy and paste these in message to mention users listed:")
+
+                out = ' '.join(mention_list)
+                for page in pagify(out, shorten_by=24):
+                    await self.bot.say(box(page))
+
+            # Display a copy-and-pastable list of ids
+            if option_output_id:
+                id_list = [m.id for m in out_members]
+                await self.bot.say(
+                    "Copy and paste these in message to mention users listed:")
+                out = ' '.join(id_list)
+                for page in pagify(out, shorten_by=24):
+                    await self.bot.say(box(page))
+
+    def get_member_csv(self, members):
+        """Return members as a list."""
+        names = [m.display_name for m in members]
+        return ', '.join(names)
+
+    def get_member_list(self, members):
+        """Return members as a list."""
+        out = []
+        for m in members:
+            out.append('+ {}'.format(m.display_name))
+        return '\n'.join(out)
+
+    def get_member_embeds(self, ctx, members):
+        """Discord embed of data display."""
+        color = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+        color = int(color, 16)
+        embeds = []
+
+        # split embed output to multiples of 25
+        # because embed only supports 25 max fields
+        out_members_group = self.grouper(25, members)
+
+        for out_members_list in out_members_group:
+            data = discord.Embed(
+                color=discord.Colour(value=color))
+            for m in out_members_list:
+                value = []
+                roles = [r.name for r in m.roles if r.name != "@everyone"]
+                value.append(', '.join(roles))
+
+                name = m.display_name
+                since_joined = (ctx.message.timestamp - m.joined_at).days
+
+                data.add_field(
+                    name=str(name),
+                    value=str(
+                        ''.join(value) +
+                        '\n{} days ago'.format(
+                            since_joined)))
+            embeds.append(data)
+        return embeds
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def listroles(self, ctx: Context, *roles):
+        """List all the roles on the server."""
+        server = ctx.message.server
+        if server is None:
+            return
+        out = []
+        out.append("__List of roles on {}__".format(server.name))
+        roles_to_list = []
+        if len(roles):
+            roles_to_list = [
+                r for r in server.roles if r.name.lower()
+                in [r2.lower() for r2 in roles]]
+        else:
+            roles_to_list = server.roles
+
+        out_roles = {}
+        for role in roles_to_list:
+            out_roles[role.id] = {'role': role, 'count': 0}
+        for member in server.members:
+            for role in member.roles:
+                if role in roles_to_list:
+                    out_roles[role.id]['count'] += 1
+        for role in server.role_hierarchy:
+            if role in roles_to_list:
+                out.append(
+                    "**{}** ({} members)".format(
+                        role.name, out_roles[role.id]['count']))
+        for page in pagify("\n".join(out), shorten_by=12):
+            await self.bot.say(page)
+
+    @commands.command(pass_context=True, no_pm=True)
+    @commands.has_any_role(*BOTCOMMANDER_ROLE)
+    async def searchmember(self, ctx, name=None):
+        """Search member on server by name."""
+        if name is None:
+            await send_cmd_help(ctx)
+            return
+
+        server = ctx.message.server
+        results = []
+        for member in server.members:
+            for member_name in [member.display_name, member.name]:
+                if name.lower() in member_name.lower():
+                    results.append(member)
+                    break
+
+        if not len(results):
+            await self.bot.say("Cannot find any users with that name.")
+            return
+
+        await self.bot.say('Found {} members.'.format(len(results)))
+
+        for member in results:
+            out = [
+                '---------------------',
+                'Display name: {}'.format(member.display_name),
+                'Username: {}'.format(str(member)),
+                'Roles: {}'.format(', '.join([r.name for r in member.roles if not r.is_everyone])),
+                'id: {}'.format(member.id)
+            ]
+            await self.bot.say('\n'.join(out))		
+
 def setup(bot):
     n = POWERHAUSRoles(bot)
     bot.add_cog(n)
