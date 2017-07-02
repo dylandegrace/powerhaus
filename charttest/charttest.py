0aeddfc75a964ecedf38631254369155d3d8da0e
dylandegrace <dylan.degrace@gmail.com>
2017-07-02 11:52:39 -0300

    charttest

---
+++ charttest/charttest.py
@@ -0,0 +1,42 @@
+import discord
+from .utils import checks
+from discord.ext import commands
+from .utils.dataIO import dataIO
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+class plottest:
+    def __init__(self, bot):
+        self.bot = bot
+		
+    @commands.command(pass_context=True, no_pm=True, name='chart')
+    async def chart(self, context):
+
+        plt.plot([1,2,3])
+
+        plot_filename = 'plot.png'
+        plot_name = ""
+        
+
+        with io.BytesIO() as f:
+            plt.savefig(
+                f, format="png", facecolor=facecolor,
+                edgecolor=edgecolor, transparent=True)
+            f.seek(0)
+            await context.bot.send_file(
+                context.message.channel,
+                f,
+                filename=plot_filename,
+                content=plot_name)
+
+        fig.clf()
+        plt.clf()
+        plt.cla()
+
+
+def setup(bot):
+    check_files()
+    n = plottest(bot)
+    bot.add_cog(n)
