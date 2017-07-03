6c8a82d170dd11352963b65c4110dcde45ea30bf
dylandegrace <dylan.degrace@gmail.com>
2017-07-03 11:05:20 -0300

    asd

--- phroles/phroles.py
+++ phroles/phroles.py
@@ -215,7 +215,7 @@ class POWERHAUSRoles:
         plot_filename = 'plot.png'
         plot_name = ""
         
-        fig.subplots_adjust(top=2.5, hspace=.35)
+        fig.subplots_adjust(top=.5, hspace=.35)
         
 
         with io.BytesIO() as f:
