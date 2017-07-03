ad51c2085a91c7fda645fa4876e5ee18f486861e
dylandegrace <dylan.degrace@gmail.com>
2017-07-03 10:53:59 -0300

    padding

--- phroles/phroles.py
+++ phroles/phroles.py
@@ -215,7 +215,7 @@ class POWERHAUSRoles:
         plot_filename = 'plot.png'
         plot_name = ""
         
-        plt.tight_layout(pad=2.08)
+        plt.tight_layout(pad=2.08,w_pad=4.5, h_pad=4.0)
         
 
         with io.BytesIO() as f:
