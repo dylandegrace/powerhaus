84c5a800553c1ca144de4e9133cda5bd181acc38
dylandegrace <dylan.degrace@gmail.com>
2017-07-03 10:57:21 -0300

    opad

--- phroles/phroles.py
+++ phroles/phroles.py
@@ -215,7 +215,7 @@ class POWERHAUSRoles:
         plot_filename = 'plot.png'
         plot_name = ""
         
-        plt.tight_layout(pad=20)
+        plt.tight_layout(pad=10)
         
 
         with io.BytesIO() as f:
