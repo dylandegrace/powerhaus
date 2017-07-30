6b4f0734abf7038f884a41c26a4120b1311b5a14
dylandegrace <dylan.degrace@gmail.com>
2017-07-30 19:49:55 -0300

    asd

--- games/games.py
+++ games/games.py
@@ -224,7 +224,7 @@ class Games:
     async def salert(self, ctx):
         """Adds stream alerts to the channel defined"""
         salert = self.bot.get_cog("Streams")
-        salert.streamalert.twitch_alert(ctx, stream : str)
+        salert.streamalert.twitch_alert(ctx, stream)
         
     @commands.group(name = "slist", pass_context=True)  
     async def slist(self, ctx):
