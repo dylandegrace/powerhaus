a345f633783bc549535d6471afd584c9fd656363
dylandegrace <dylan.degrace@gmail.com>
2017-07-30 19:33:32 -0300

    testagain

--- games/games.py
+++ games/games.py
@@ -223,7 +223,7 @@ class Games:
     @commands.command(name = "salert", pass_context=True)
     async def salert(self, ctx):
         """Adds stream alerts to the channel defined"""
-        salert = self.bot.get_cog("Streams")
+        self.bot.get_cog("Streams")
         
     @commands.group(name = "slist", pass_context=True)  
     async def slist(self, ctx):
